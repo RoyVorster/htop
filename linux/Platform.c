617476e2a4616f269ef5a6631b6a8cc582c31014
Roy Vorster <royvorster@gmail.com>
2018-09-08 15:15:39 +0200

    Remove unecessary changes

--- linux/Platform.c
+++ linux/Platform.c
@@ -191,8 +191,6 @@ double Platform_setCPUValues(Meter* this, int cpu) {
       percent = v[0]+v[1]+v[2]+v[3];
    }
 
-   v[CPU_METER_CLOCKFREQUENCY] = cpuData->clockFrequency;
-
    percent = CLAMP(percent, 0.0, 100.0);
    if (isnan(percent)) percent = 0.0;
    return percent;
